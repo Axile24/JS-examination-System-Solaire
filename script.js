55d53fb9852a9801e25f1892b9cb92b4b45f8cb5
Axile24 <amokrane.n@icloud.com>
2024-12-04 00:35:09 +0100

    bytt css och JS api request funakr i consolen

---
+++ script.js
@@ -0,0 +1,31 @@
+//Main js module
+
+import {getPlanet} from "./requestApi.js"
+import {overlay, planetFigures, sunSlice} from "./domElements.js";
+import {displayPlanetData, hideOverlay, setPlanetSizes} from "./domManipulation.js";
+
+
+
+setPlanetSizes()
+
+
+for (const planet of planetFigures) {
+    planet.addEventListener('click', async() => {
+
+        const planetName = planet.getAttribute('alt')
+        const planetData = await getPlanet(planetName)
+
+        displayPlanetData(planetData)
+        
+    })
+}
+
+sunSlice.addEventListener('click', async() => {
+    const sunData = await getPlanet('Solen')
+    displayPlanetData(sunData)
+})
+
+
+overlay.modal.addEventListener('click', () => {
+    hideOverlay()
+})
